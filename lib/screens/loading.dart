71f74a2104e4d60ad9c7a9cfc52eec2034c31d6c
Nader Elziny <naderelziny805@gmail.com>
2022-11-14 20:12:18 +0300

    Refactored the way of handling getting location.

--- lib/screens/loading.dart
+++ lib/screens/loading.dart
@@ -9,31 +9,13 @@ class LoadingScreen extends StatefulWidget {
 }
 
 void findLocation() async {
-  LocationPermission permission;
-
-  permission = await Geolocator.checkPermission();
-  if (permission == LocationPermission.denied) {
-    permission = await Geolocator.requestPermission();
-    if (permission == LocationPermission.denied) {
-      // Permissions are denied, next time you could try
-      // requesting permissions again (this is also where
-      // Android's shouldShowRequestPermissionRationale
-      // returned true. According to Android guidelines
-      // your App should show an explanatory UI now.
-      return Future.error('Location permissions are denied');
-    }
+  try {
+    Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high);
+    print(position);
+  } catch (e) {
+    print('Accessing location request denied.');
   }
-
-  if (permission == LocationPermission.deniedForever) {
-    // Permissions are denied forever, handle appropriately.
-    return Future.error(
-        'Location permissions are permanently denied, we cannot request permissions.');
-  }
-
-  Position position = await Geolocator.getCurrentPosition(
-      desiredAccuracy: LocationAccuracy.high);
-
-  print(position);
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
