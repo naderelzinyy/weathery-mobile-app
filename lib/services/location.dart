5a144b6e73a16aa8881a2c07b063878d547c630e
Nader Elziny <naderelziny805@gmail.com>
2022-11-17 00:31:33 +0300

    Handled requesting a location access from the user.

--- lib/services/location.dart
+++ lib/services/location.dart
@@ -6,6 +6,21 @@ class Location {
   late Position position;
 
   Future<void> findLocation() async {
+    LocationPermission permission;
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        return Future.error('Location permissions are denied');
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      // Permissions are denied forever, handle appropriately.
+      return Future.error(
+          'Location permissions are permanently denied, we cannot request permissions.');
+    }
+
     try {
       Position position = await Geolocator.getCurrentPosition(
           desiredAccuracy: LocationAccuracy.high);
